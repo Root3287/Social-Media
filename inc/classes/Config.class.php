42b64b884d9e18512cabfffbc263f11eb6738a27
Timothy Gibbons <tjgibbons@gmail.com>
2016-02-02 13:30:56 -0600

    Fix config. Fix config class to don't show errors when no config has been loaded.

--- inc/classes/Config.class.php
+++ inc/classes/Config.class.php
@@ -2,7 +2,7 @@
 class Config{
 	public static function get($path = null) {
 		if($path) {
-			$config = $GLOBALS['config'];
+			$config = (isset($GLOBALS['config']))? $GLOBALS['config']:null;
 			$path = explode('/', $path);
 			foreach($path as $bit){
 				if(isset($config[$bit])) {
