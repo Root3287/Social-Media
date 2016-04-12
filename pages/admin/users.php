9030e5711df406305a35b6f8f2aee4ae1acccbe0
Timothy Gibbons <Root3287@users.noreply.github.com>
2016-04-12 12:17:26 -0500

    Update users.php

--- pages/admin/users.php
+++ pages/admin/users.php
@@ -1,17 +1,12 @@
 <?php
-/*
- * TODO: Paginate user
- */
 $user = new User();
-$db = DB::getInstance();
-if($user->isAdmLoggedIn()){
+if($user->isAmdLoggedIn()){
 	if($user->data()->group != 2){
-		Redirect::to('/');
+		Redirect::to('/admin/login/');
 	}
 }else{
-	Redirect::to('/admin');
+	Redirect::to('/admin/login/');
 }
-
 $f = null;
 if(Input::exists()){
 	If(Token::check(Input::get('token'))){
@@ -21,121 +16,137 @@ if(Input::exists()){
 		));
 		if($validation->passed()){
 			$q = escape(Input::get('search'));
-			$t = Input::get('option');
+			$t = escape(Input::get('option'));
 			$f= $db->query("SELECT * FROM `users` WHERE {$t} LIKE '%{$q}%'")->results();
 		}
 	}
 }
+$page = (Input::get('p') !=null)?Input::get('p'):1;
+$limit = (Input::get('l') !=null)? Input::get('l'):10;
+
+$pagination = new PaginateArray($f);
+$userData = $pagination->getArrayData($limit, $page);
 ?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<?php require 'assets/head.php';?> 
+		<?php require 'assets/head.php';?>
 	</head>
 	<body>
 		<?php require 'assets/nav.php';?>
 		<div class="container">
-		<div class="col-md-3"><?php require 'pages/admin/sidebar.php';?></div>
-		<div class="col-md-9">
-			<form class="form-inline" method="post" action="/admin/users">
-				<div class="form-group">
-					<input name="search" type="text" class="form-control input-md" placeholder="Search">
+			<div class="row">
+					<form class="form-inline" method="post" action="/admin/users/">
+					<div class="form-group">
+						<input name="search" type="text" class="form-control input-md" placeholder="Search">
+					</div>
+					<div class="form-group">
+						<select name="option">
+							<option value="id">
+								ID
+							</option>
+							<option selected="selected" value="username">
+								Username
+							</option>
+							<option value="name">
+								Name
+							</option>
+							<option value="email">
+								Email
+							</option>
+						</select>
+					</div>
+					<div class="form-group">
+						<input type="hidden" name="token" value="<?php echo Token::generate()?>">
+						<input class="btn btn-md btn-primary" type="submit" value="find">
+					</div>
+				</form>
+			</div>
+			<div class="row">
+				<div class="col-md-3"><?php require 'pages/admin/sidebar.php';?></div>
+				<div class="col-md-9">
+					<table class="table">
+						<thead>
+							<tr>
+								<td>
+									ID
+								</td>
+								<td>
+									Name
+								</td>
+								<td>
+									Username
+								</td>
+								<td>
+									Email
+								</td>
+								<td>
+									Group
+								</td>
+								<td>
+									Joined
+								</td>
+								<td>
+									Action
+								</td>
+							</tr>
+						</thead>
+						<tbody>
+							<?php if($f == null):?>
+								<?php foreach($userData as $users):?>
+								<tr>
+									<td>
+										<?php echo $users->id?>
+									</td>
+									<td>
+										<?php echo $users->name?>
+									</td>
+									<td>
+										<?php echo $users->username?>
+									</td>
+									<td>
+										<?php echo $users->email?>
+									</td>
+									<td>
+										<?php echo $users->group?>
+									</td>
+									<td>
+										<?php echo $users->joined?>
+									</td>
+								</tr>
+								<?php endforeach;?>
+							<?php else:?>
+								<?php foreach ($userData as $users):?>
+								<tr>
+									<td>
+										<?php echo $users->id?>
+									</td>
+									<td>
+										<?php echo $users->name?>
+									</td>
+									<td>
+										<?php echo $users->username?>
+									</td>
+									<td>
+										<?php echo $users->email?>
+									</td>
+									<td>
+										<?php echo $users->joined?>
+									</td>
+								</tr>
+								<?php endforeach;?>
+							<?php endif;?>
+						</tbody>
+					</table>	
 				</div>
-				<div class="form-group">
-					<select name="option">
-						<option value="id">
-							ID
-						</option>
-						<option selected="selected" value="username">
-							Username
-						</option>
-						<option value="name">
-							Name
-						</option>
-						<option value="email">
-							Email
-						</option>
-					</select>
-				</div>
-				<div class="form-group">
-					<input type="hidden" name="token" value="<?php echo Token::generate()?>">
-					<input class="btn btn-md btn-primary" type="submit" value="find">
-				</div>
-			</form>
-			<table class="table">
-				<thead>
-					<tr>
-						<td>
-							ID
-						</td>
-						<td>
-							Name
-						</td>
-						<td>
-							Username
-						</td>
-						<td>
-							Email
-						</td>
-						<td>
-							Group
-						</td>
-						<td>
-							Joined
-						</td>
-						<td>
-							Action
-						</td>
-					</tr>
-				</thead>
-				<tbody>
-					<?php if($f == null):?>
-						<?php foreach($db->get('users', array('1','=','1'))->results() as $users):?>
-						<tr>
-							<td>
-								<?php echo $users->id?>
-							</td>
-							<td>
-								<?php echo $users->name?>
-							</td>
-							<td>
-								<?php echo $users->username?>
-							</td>
-							<td>
-								<?php echo $users->email?>
-							</td>
-							<td>
-								<?php echo $users->group?>
-							</td>
-							<td>
-								<?php echo $users->joined?>
-							</td>
-						</tr>
-						<?php endforeach;?>
-					<?php else:?>
-						<?php foreach ($f as $users):?>
-						<tr>
-							<td>
-								<?php echo $users->id?>
-							</td>
-							<td>
-								<?php echo $users->name?>
-							</td>
-							<td>
-								<?php echo $users->username?>
-							</td>
-							<td>
-								<?php echo $users->email?>
-							</td>
-							<td>
-								<?php echo $users->joined?>
-							</td>
-						</tr>
-						<?php endforeach;?>
-					<?php endif;?>
-				</tbody>
-			</table>	
-		</div>
+			</div>
+			<div class="row">
+				<ul class="pagination">
+						<?php for($i = 1; $i<=$pagination->getTotalPages(); $i++):?>
+							<li><a href="?p=<?php echo $i?>"><?php echo $i;?></a></li>
+						<?php endfor; ?>
+					</ul>
+			</div>
 		</div>
 		<?php require 'assets/foot.php';?>
 	</body>
