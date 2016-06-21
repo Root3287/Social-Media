25be366dfbf9b28cbde05f623f733628912e1578
Timothy Gibbons <tjgibbons10@gmail.com>
2016-06-21 15:16:09 -0500

    Returned SelfUrl() Undo the deletion of SelfUrl()

--- inc/functions.php
+++ inc/functions.php
@@ -57,3 +57,14 @@ function getClientIP(){
     }
     return $ip;
 }
+function getSelfUrl(){
+    if($_SERVER['SERVER_ADDR'] !== "127.0.0.1"){
+        if($_SERVER['SERVER_PORT'] == 80){
+            return $_SERVER['REQUEST_SCHEME']."://".$_SERVER['SERVER_NAME'];
+        }else{
+            return $_SERVER['REQUEST_SCHEME']."://".$_SERVER['SERVER_NAME'].":".$_SERVER['SERVER_PORT'];
+        }
+    }else{
+        return false;
+    }
+}
